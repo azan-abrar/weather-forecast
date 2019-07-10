53bf8f67428a48f8d87be8c6b70f640b1bb8ad5d
Azan Abrar <azan.abrar@gmail.com>
2019-07-10 12:09:58 +0500

    weather forecast for current location

---
+++ app/controllers/application_controller.rb
@@ -0,0 +1,31 @@
+class ApplicationController < ActionController::Base
+  require 'forecast_io'
+  require 'open-uri'
+
+  def show_weather
+    begin
+      current_ip = open('http://whatismyip.akamai.com').read
+      location = Geocoder.search(current_ip)
+      longlat = location.last.data.dig('loc').split(',')
+      longitude = longlat.first
+      latitude = longlat.last
+      forecast = ForecastIO.forecast(longitude, latitude)
+      weather =JSON.parse(forecast.to_json)
+      @timezone = weather['timezone']
+      @data = weather['currently']
+    rescue
+      redirect_to error_path
+    end
+  end
+
+  def show_error
+    @error = "Please retry there is an issue with connection"
+  end
+
+  private
+    def farenheit_to_celcius(farenheit)
+      (farenheit - 32) * (5.to_f/9.to_f)
+    end
+
+    helper_method :farenheit_to_celcius
+end
---
+++ config/initializers/application_controller_renderer.rb
@@ -0,0 +1,9 @@
+# Be sure to restart your server when you modify this file.
+
+# ActiveSupport::Reloader.to_prepare do
+#   ApplicationController.renderer.defaults.merge!(
+#     http_host: 'example.org',
+#     https: false
+#   )
+# end
+ForecastIO.api_key = '6f333db4f6d826d486cd960d64c5db2f'
